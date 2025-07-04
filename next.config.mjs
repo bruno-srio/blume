2c4d9e2ca1e04728b026f3b07b537c12057735ab
Bruno Ribeiro <contatobrunosrio@gmail.com>
2025-07-04 18:56:20 +0100

    chore(next): switch images.domains to remotePatterns to fix deprecation warning

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        "uploadthing.com",
-        "utfs.io",
-        "img.clerk.com",
-        "subdomain",
-        "files.stripe.com",
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "uploadthing.com" },
+      { protocol: "https", hostname: "utfs.io" },
+      { protocol: "https", hostname: "img.clerk.com" },
+      { protocol: "https", hostname: "subdomain" }, // fix this eventually
+      { protocol: "https", hostname: "files.stripe.com" },
+    ],
+  },
+};
+
+export default nextConfig;
