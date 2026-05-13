fe75c779a8e2405044c049cbaf42a6e3ee32a80a
Bruno Ribeiro <contatobrunosrio@gmail.com>
2026-05-12 21:20:20 -0300

    chore(agency): refine create agency page heading copy

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -33,7 +33,7 @@ const Page = async ({searchParams}: {
   return (
     <div className='flex justify-center items-center mt-4'>
       <div className='max-w-[850px] border-[1px] p-4 rounded-xl'>
-        <h1 className='text-4xl'>Create An Agency</h1>
+        <h1 className='text-4xl'>Create an agency for your business</h1>
         <AgencyDetails data = {{companyEmail: authUser?.emailAddresses[0].emailAddress}} />
       </div>
     </div>
