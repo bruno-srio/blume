45c230bb236bb9370514e113bd482ca8b1907204
Bruno Ribeiro <contatobrunosrio@gmail.com>
2026-07-08 20:46:25 +0100

    feat(layout): add agency layout auth and redirect guards

--- src/app/(main)/agency/[agencyId]/layout.tsx
+++ src/app/(main)/agency/[agencyId]/layout.tsx
@@ -1,11 +1,30 @@
+import { verifyAndAcceptInvitation } from '@/lib/queries'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  children: React.ReactNode
+  params: {
+    agencyId: string
+  }
+}
 
-const layout = (props: Props) => {
-  return (
-    <div>layout</div>
-  )
+const layout = async ({ children, params }: Props) => {
+  const agencyId = await verifyAndAcceptInvitation()
+  const user = await currentUser()
+
+  if (!user) {
+    return redirect('/')
+  }
+
+  if (!agencyId) {
+    return redirect('/agency')
+  }
+
+  if (user.privateMetadata.role !== 'AGENCY_OWNER' && user.privateMetadata.role !== 'AGENCY_ADMIN') {
+    // return unauthorized page WIP
+  }
 }
 
 export default layout
