05b3c2ecee55472f03904202e220493df59a88a8
Bruno Ribeiro <contatobrunosrio@gmail.com>
2026-07-30 17:31:13 +0100

    feat(layout): mount ModalProvider and Sonner toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import ModalProvider  from "@/providers/modal-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const font = DM_Sans({ subsets: ["latin"] });
 
@@ -20,7 +22,10 @@ export default function RootLayout({
 
         <body className={font.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            {children}
+            <ModalProvider>
+              {children}
+              <Toaster />
+            </ModalProvider>
           </ThemeProvider>
         </body>
 
